d383bbf82c32d96c464d7adc9ad84e8ea4ba3617
FKAYSR <kejserfreja@gmail.com>
2026-03-18 11:38:42 +0100

    fix audio file paths and ensure proper file extensions

--- src/audio/audio.jsx
+++ src/audio/audio.jsx
@@ -11,10 +11,10 @@ export default function playSound(name, volume = 0.5) {
 }
 
 const sounds = {
-    start: "../sounds/game-start.mp3",
-    slowMusic: "../sounds/slow-music.mp3",
-    gameMusic: "../sounds/game-music.mp3",
-    choose: "../sounds/choose.mp3",
-    restart: "../sounds/restart.mp3",
-    success: "../sounds/success"
+    start: "/sounds/game-start.mp3",
+    slowMusic: "/sounds/slow-music.mp3",
+    gameMusic: "/sounds/game-music.mp3",
+    choose: "/sounds/choose.mp3",
+    restart: "/sounds/restart.mp3",
+    success: "/sounds/success.mp3"
 };
