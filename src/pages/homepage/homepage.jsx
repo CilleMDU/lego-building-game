378a03a0b615688be5728a751d916c4b46401f33
CilleMDU <eaa25cesc@students.eaaa.dk>
2026-03-18 11:28:19 +0100

    homepage completed and roots added

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,11 +1,18 @@
 import styles from "./homepage.module.css";
 import StartBtn from "../../assets/startBtn.svg";
+import Logo from "../../assets/legoLogo.svg";
 
 export default function Homepage() {
     return (
         <div className={styles.homepage}>
-            <h1 className={styles.title}>Building Competition</h1>
-            <p className={styles.description}>Denmark VS Netherlands</p>
+            <div className={styles.titleContainer}>
+                <img src={Logo} alt="Lego Logo" className={styles.logo}/>
+                <h1 className={styles.title}>Building <br /> Competition</h1>
+            </div>
+            <div className={styles.descriptionContainer}>
+                <p className={styles.description}>Denmark VS Netherlands</p>
+                <p className={styles.edition}>EDITION</p>
+            </div>
             <button className={styles.startBtn} onClick={() => window.location.href="/languageselect"}>
                 <img src={StartBtn} alt="Start Game"/>
             </button>
