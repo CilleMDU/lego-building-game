d59c18a36a69cf3837e95643108f835cbffd0906
FKAYSR <kejserfreja@gmail.com>
2026-03-18 11:28:40 +0100

    add audio files and implement playSound function

---
+++ src/audio/audio.jsx
@@ -0,0 +1,12 @@
+export default function playSound(name, volume = 0.5) {
+    const src =
+}
+
+const sounds = {
+    start: "../sounds/game-start.mp3",
+    slowMusic: "../sounds/slow-music.mp3",
+    gameMusic: "../sounds/game-music.mp3",
+    choose: "../sounds/choose.mp3",
+    restart: "../sounds/restart.mp3",
+    success: "../sounds/success"
+};
